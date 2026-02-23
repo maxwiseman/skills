67cc8911b48cb3d9f25d8e6b30822bd8a264ec23
maxwiseman <max@maxwiseman.io>
2026-02-23 10:33:09 -0500

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@skills/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@skills/db";
+import * as schema from "@skills/db/schema/auth";
+import { env } from "@skills/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
